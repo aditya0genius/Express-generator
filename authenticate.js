98a16e6c815a2fa1a91c83c5062b1fa64388636b
Aditya Deshmukh <adityadeshmukh168@gmail.com>
2020-07-21 14:55:36 +0530

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -6,6 +6,7 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var config = require('./config.js');
+const { NotExtended } = require('http-errors');
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -15,6 +16,20 @@ exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey,
         {expiresIn: 3600});
 };
+
+exports.verifyAdmin = function(req, res, next){
+    if(req.user.admin === false)
+    {
+        err = new Error('You are not authorized to perform this operation!');
+        res.statusCode = 403;
+        next(err);
+    }
+    else
+    {
+        next();
+    }
+};
+
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,16 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/",authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    User.find({}, (err, users) => {
+      if (err) {
+        return next(err);
+      } else {
+        res.json(users);
+      }
+    });
+  }
+);
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
