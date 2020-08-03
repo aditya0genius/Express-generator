b3cef786d5d20ffff4620cd414f6f574643f43ad
Aditya Deshmukh <adityadeshmukh168@gmail.com>
2020-08-03 19:05:10 +0530

    Facebook Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '286964042363918',
+        clientSecret: '3c73326c7c2d80a549c2be117014d5e3'
+    }
 }
